1084493fb5235270d7f89b113c3d0b6fc4a30f4d
Yago Marques <44405378+yagobmarques@users.noreply.github.com>
2019-09-26 16:17:24 -0300

    Update main.js

--- CadastroWeb/main.js
+++ CadastroWeb/main.js
@@ -1,42 +1,22 @@
-var Pessoa ={
-    primeiroNome: "Nome",
-    ultimoNome: "Ultimo nome",
-    nascimento: new Date(),
-    altura: 0.0,
-    peso: 0.0,
-    sexo: "Indefinido",
-    // Pessoa(primeiroNome, ultimoNome, nascimento, altura, peso, sexo){
-    //     this.primeiroNome = primeiroNome;
-    //     this.ultimoNome = ultimoNome;
-    //     this.nascimento = nascimento;
-    //     this.altura = altura;
-    //     this.peso = peso;
-    //     this.sexo = sexo;
-    // },
-     nomeCompleto() {
-        return this.primeiroNome + " "+ this.ultimoNome;
-    },
-    
-     idadeAtual() {
-        return 2018-this.nascimento.getFullYear();
-    },
-    
-     dataNascimento() {
-        return this.nascimento.getFullYear()+"-"+this.nascimento.getMonth()+"-"+this.nascimento.getDay();
-    },
-     imc() {
-         return this.peso/(this.altura*this.altura);    
-    },
-};
-function Pessoa(){
-        this.primeiroNome = primeiroNome;
-        this.ultimoNome = ultimoNome;
-        this.nascimento = nascimento;
-        this.altura = altura;
-        this.peso = peso;
-        this.sexo = sexo;
-}
 var Lista = [];
+
+function idadeAtual(nascimento) {
+  return 2018-nascimento.slice(0,4);
+}
+
+function nomeCompleto(primeiroNome, segundoNome) {
+    return primeiroNome + " "+segundoNome;
+}
+
+function dataNascimento(nascimento) {
+  dados = nascimento.split("-");
+  return dados[2]+ "/"+dados[1]+"/"+dados[0];
+}
+
+function imc(peso, altura) {
+  return peso/(altura*altura);    
+}
+
 function cadastrar(){
     var pessoinha = {primeiroNome: "Nome",  ultimoNome: "Ultimo nome", nascimento: new Date(),altura: 0.0,peso: 0.0,sexo: "Indefinido"};
     pessoinha.primeiroNome = document.getElementById("nome").value;
@@ -45,10 +25,20 @@ function cadastrar(){
     pessoinha.altura = document.getElementById("altura").value;
     pessoinha.peso  = document.getElementById("peso").value;
     pessoinha.sexo = document.getElementById("sexo").value;
-    //var Lista = [];
     Lista.push(pessoinha);
-    alert(Lista.length);
-    for (let index = 0; index < Lista.length; index++) {
-        console.log(Lista[index].primeiroNome);
-    }
+}
+
+function mostrar(){
+  for (i = 0 ; i < Lista.length; i++){
+        nome = "<td>" + nomeCompleto(Lista[i].primeiroNome,Lista[i].ultimoNome) + "</td>";
+        idade = "<td>" + idadeAtual(Lista[i].nascimento) + "</td>";
+        sexo = "<td>" + Lista[i].sexo + "</td>";
+        nascimento = "<td>" + dataNascimento(Lista[i].nascimento) + "</td>";
+        imc1 = "<td>" + imc(Lista[i].peso, Lista[i].altura) + "</td>";
+        var t = document.getElementById("tabela1");
+        var r = document.createElement("TR");               
+        r.innerHTML = nome + idade + sexo + nascimento + imc1;
+        //document.querySelector("table").appendChild("<tr>" + nome + idade + sexo + nascimento + imc + "/tr>"); 
+        t.appendChild(r);
+  }
 }
